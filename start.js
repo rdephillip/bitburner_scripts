d9bc13060b452b96ffd0cf0b2b38ff200811c0c7
TheBarbarian_CNX <943336+rdephillip@users.noreply.github.com>
2023-12-20 18:12:02 -0500

    Create start.js

---
+++ start.js
@@ -0,0 +1,34 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  let servers = [];
+
+  if (!ns.fileExists("servers.txt")){
+    ns.run("claim.js");
+  }
+  
+  servers = ns.read("servers.txt").split("\n");
+
+  let scriptName = ns.args[0];
+  let targetName = ns.args[1];
+  let forceUpdate = ((ns.args.length === 3) ? ns.args[2] : false);
+
+  for (let server of servers) {
+    if (server){
+      if(ns.hasRootAccess(server)) {
+        if(!ns.fileExists(scriptName, server) || forceUpdate) {
+          ns.toast("Copying to " + server);
+          ns.scp(scriptName, server);
+        }
+
+        if(!ns.scriptRunning(scriptName, server)) {
+          let freeRAM = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
+          let threadCount = Math.floor(freeRAM / ns.getScriptRam(scriptName));
+          if (threadCount > 0){
+            ns.exec(scriptName, server, threadCount, targetName);
+          }
+        }
+      }
+      await ns.sleep(0);
+    }
+  }
+}
