d50fb0673b0b30b72099c48caa5c0dc687dfbfb1
TheBarbarian_CNX <943336+rdephillip@users.noreply.github.com>
2023-12-20 18:59:39 -0500

    Update gwh.js

--- fresh-start/gwh.js
+++ fresh-start/gwh.js
@@ -1,9 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
   while(true) {
-    ns.toast(ns.args[0] + ' has ' +ns.getServerMoneyAvailable(ns.args[0]) + ' out of ' + ns.getServerMaxMoney(ns.args[0]) + ' money.', "info")
-    ns.toast(ns.args[0] + ' security is at ' + ns.getServerSecurityLevel(ns.args[0]) + ' with a min sec of ' + ns.getServerMinSecurityLevel(ns.args[0]) + '.', "info")
-
     if (ns.getServerSecurityLevel(ns.args[0]) > ns.getServerMinSecurityLevel(ns.args[0])){
       ns.toast('Weakening security on' + ns.args[0] + '.', "warning")
       let secWeakened = await ns.weaken(ns.args[0]);
