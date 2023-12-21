1595cf47f815539e40e23a03c2759d2a4c207ea2
TheBarbarian_CNX <943336+rdephillip@users.noreply.github.com>
2023-12-20 19:38:08 -0500

    Update start.js

--- fresh-start/start.js
+++ fresh-start/start.js
@@ -10,28 +10,42 @@ export async function main(ns) {
 
   let scriptName = ns.args[0];
   let targetName = ns.args[1];
-  let forceUpdate = ((ns.args.length > 2) ? ns.args[2] : false);
+  let forceUpdate = ((ns.args.length > 2) ? ns.args[2] : "");
+
+  if (ns.args[2] === "restart") {
+    restart(ns, scriptName, servers);
+  }
 
   for (let server of servers) {
     if (server){
       if(ns.hasRootAccess(server)) {
-        if(!ns.fileExists(scriptName, server) || forceUpdate) {
-          ns.toast("Copying to " + server);
+        if(!ns.fileExists(scriptName, server) || forceUpdate === "file") {
+          ns.toast("Copying to " + server, "success");
           ns.scp(scriptName, server);
         }
 
         let freeRAM = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
         let threadCount = Math.floor(freeRAM / ns.getScriptRam(scriptName));
-
-        if(ns.scriptRunning(scriptName, server)) {
-          ns.scriptKill(scriptName, server);
-        }
         
-        if (threadCount > 0){
-            ns.exec(scriptName, server, threadCount, targetName);
-        }
+        start(ns, scriptName, server, threadCount, targetName);
       }
       await ns.sleep(0);
     }
+  }
+}
+
+function restart(ns, scriptName, servers) {
+  for (let server of servers) {
+    if (server) {
+      ns.scriptKill(scriptName, server);
+      ns.toast("Killed " + scriptName + " on " + server, "error");
+    }
+  }
+}
+
+function start(ns, scriptName, server, threadCount, targetName) {
+  if (threadCount > 0) {
+    ns.exec(scriptName, server, threadCount, targetName);
+    ns.toast("Started " + scriptName + " on " + server, "success");
   }
 }
