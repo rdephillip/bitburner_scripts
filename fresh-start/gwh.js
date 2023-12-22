1972ba3ad1441f41136a256a36a2edc482856881
TheBarbarian_CNX <943336+rdephillip@users.noreply.github.com>
2023-12-22 13:30:44 -0500

    Update gwh.js

--- fresh-start/gwh.js
+++ fresh-start/gwh.js
@@ -1,17 +1,23 @@
 /** @param {NS} ns */
 export async function main(ns) {
+  let server = ns.args[0];
+  let threadCount = ns.args[1];
+  let estGrowthAmt = 0;
+
   while(true) {
-    if (ns.getServerSecurityLevel(ns.args[0]) > ns.getServerMinSecurityLevel(ns.args[0])){
-      ns.toast('Weakening security on' + ns.args[0] + '.', "warning")
-      let secWeakened = await ns.weaken(ns.args[0]);
+    estGrowthAmt = (ns.getServerMoneyAvailable(server) * (1 + (ns.getServerGrowth(server) / 100)));
+    
+    if (ns.getServerSecurityLevel(server) > ns.getServerMinSecurityLevel(server) && Math.floor(ns.weakenAnalyze(threadCount)) > 1){
+      ns.toast('Weakening security on ' + server + '.', "warning")
+      let secWeakened = await ns.weaken(server);
       ns.toast('Weakened by ' + secWeakened + '.', "success");
-    } else if (ns.getServerMoneyAvailable(ns.args[0]) < ns.getServerMaxMoney(ns.args[0])){
-      ns.toast('Growing funds on ' + ns.args[0] + '.', "warning")
-      let moneyInc = await ns.grow(ns.args[0]);
+    } else if (ns.getServerMoneyAvailable(server) < ns.getServerMaxMoney(server) && (ns.getServerMaxMoney(server) - estGrowthAmt > estGrowthAmt )) {
+      ns.toast('Growing funds on ' + server + '.', "warning")
+      let moneyInc = await ns.grow(server);
       ns.printf('Funds grew ' + moneyInc + '.', "success")
-    } else {
-      ns.toast('Hacking funds on ' + ns.args[0] + '.', "warning")
-      let earnedMoney = await ns.hack(ns.args[0]);
+    } else if (ns.hackAnalyze(server) * threadCount < ns.getServerMoneyAvailable(server)) {
+      ns.toast('Hacking funds on ' + server + '.', "warning")
+      let earnedMoney = await ns.hack(server);
       ns.toast('Funds earned ' + earnedMoney + '.', "success");
     }
   }
