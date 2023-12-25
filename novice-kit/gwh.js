28f2af569d0bef7a7b0c3a197c40c80818bc3066
TheBarbarian_CNX <943336+rdephillip@users.noreply.github.com>
2023-12-25 14:25:14 -0500

    Update gwh.js

--- novice-kit/gwh.js
+++ novice-kit/gwh.js
@@ -10,15 +10,15 @@ export async function main(ns) {
     if (ns.getServerSecurityLevel(server) > ns.getServerMinSecurityLevel(server) && Math.floor(ns.weakenAnalyze(threadCount)) > 1){
       ns.toast('Weakening security on ' + server + '.', "warning")
       let secWeakened = await ns.weaken(server);
-      ns.toast('Weakened by ' + secWeakened + '.', "success");
+      ns.toast('Weakened by ' + ns.formatNumber(secWeakened, 2) + '.', "success");
     } else if (ns.getServerMoneyAvailable(server) < ns.getServerMaxMoney(server) && ns.getServerMaxMoney(server) - ns.getServerMoneyAvailable(server) > estGrowthAmt ) {
       ns.toast('Growing funds on ' + server + '.', "warning")
       let moneyInc = await ns.grow(server);
-      ns.printf('Funds grew ' + moneyInc + '.', "success")
+      ns.printf('Funds grew $' + ns.formatNumber(moneyInc, 2) + '.', "success")
     } else if (ns.hackAnalyze(server) * threadCount < ns.getServerMoneyAvailable(server)) {
       ns.toast('Hacking funds on ' + server + '.', "warning")
       let earnedMoney = await ns.hack(server);
-      ns.toast('Funds earned ' + earnedMoney + '.', "success");
+      ns.toast('Funds earned $' + ns.formatNumber(earnedMoney, 2) + '.', "success");
     }
   }
 }
