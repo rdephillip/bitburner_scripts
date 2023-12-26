cbf982de8fe11c88416cf22105d17c83b6aba0e6
TheBarbarian_CNX <943336+rdephillip@users.noreply.github.com>
2023-12-26 11:01:01 -0500

    Create auto_restart.js

---
+++ novice-kit/auto_restart.js
@@ -0,0 +1,29 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  let ownedProgs = [];
+  let programs = ["BruteSSH.exe", "FTPCrack.exe", "relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe"]
+  let setArgs = [];
+
+  if (ns.fileExists("args.txt")) {
+    setArgs = ns.read("args.txt").split("\n");
+  }
+
+  for (let p of programs) {
+    ns.tprint(p);
+    if (ns.fileExists(p) && !ownedProgs.includes(p)){
+      ownedProgs.push(p);
+    }
+  }
+
+  while (ownedProgs.length < programs.length) {
+    for (let p in programs) {
+      if (ns.fileExists(p) && !ownedProgs.includes(p)){
+        ownedProgs.push(p);
+        ns.exec("start.js", setArgs[0], setArgs[1], "restart");
+      }
+      await ns.sleep(0);
+    }
+
+    await ns.sleep(10000);
+  }
+}
