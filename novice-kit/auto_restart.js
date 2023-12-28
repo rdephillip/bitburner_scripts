01cad6712b25d2408c24588cbe8fd9ae4c6d0035
TheBarbarian_CNX <943336+rdephillip@users.noreply.github.com>
2023-12-28 17:14:11 -0500

    Update auto_restart.js

--- novice-kit/auto_restart.js
+++ novice-kit/auto_restart.js
@@ -3,6 +3,7 @@ export async function main(ns) {
   let ownedProgs = [];
   let programs = ["BruteSSH.exe", "FTPCrack.exe", "relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe"]
   let setArgs = [];
+  let lastChecked = ns.fileExists("lastCheckedHLVL.txt") ? parseInt(ns.read("lastCheckedHLVL.txt")) : 0;
 
   if (ns.fileExists("args.txt")) {
     setArgs = ns.read("args.txt").split("\n");
@@ -22,6 +23,16 @@ export async function main(ns) {
       await ns.sleep(0);
     }
 
+    await ns.sleep(10000);
+  }
+
+  while (ns.getHackingLevel() < 2500) {
+    if (ns.getHackingLevel() % 100 == 0 && ns.getHackingLevel() > lastChecked) {
+      ns.run("start.js", 1, setArgs[0], setArgs[1], "restart");
+    }
+
+    lastChecked = ns.getHackingLevel();
+    ns.write("lastCheckedHLVL.txt", lastChecked, "w");
     await ns.sleep(10000);
   }
 }
